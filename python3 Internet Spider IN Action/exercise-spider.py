278e933bdb8469ad531550b0d3082420259daf55
Geyixin <geyixin777@163.com>
2018-10-07 20:58:24 +0800

    2018-10-7 20:58:20

--- python3 Internet Spider IN Action/exercise-spider.py
+++ python3 Internet Spider IN Action/exercise-spider.py
@@ -103,10 +103,108 @@ requests 中的cookie
 #     print(key + '=' + value)
 #
 
-from pyquery import PyQuery as pq
-res = pq(html)
-# res = pq(url='https://www.baidu.com')
-print(res('li'))
+# from pyquery import PyQuery as pq
+# res = pq(html)
+# # res = pq(url='https://www.baidu.com')
+# print(res('li'))
+
+'''
+json
+'''
+# import json
+#
+# str = '[{"a":1,"b":2},{"A":11,"B":22}]'
+# print(type(str))
+# data = json.loads(str)  # json编码
+# print(data)
+# print(type(data))
+# print(data[0]['a'])
+# print(data[1].get('A'))
+# print(data[1].get('A', 2))
+# print(data[1].get('C', 2))
+# da = json.dumps(data)   # json解码
+# print(da)
+# print(type(da))
+
+# import json
+#
+# str = '[{"a":1,"b":2},{"A":11,"B":22}]'
+# with open('data.json', 'w', encoding='utf-8') as f:
+#     f.write(json.dumps(str, indent=2, ensure_ascii=False))  # indent=2,缩进两个字符
+
+'''
+CSV
+'''
+#
+# import csv
+#
+# with open('data.csv', 'w', newline='', encoding='utf-8') as cv:
+#     writer = csv.writer(cv, delimiter=' ')
+#     writer.writerow(['id', 'name', 'age'])
+#     writer.writerow(['1', 'tom', '12'])
+#     writer.writerow(['2', 'mike', '13'])
+#     writer.writerows([['3', 'tt', '12'],['4', 'mm', '12']])
+
+
+# import csv
+#
+# with open('data.csv', 'r', encoding='utf-8') as cv:
+#     read = csv.reader(cv)
+#     for row in read:
+#         print(row)
+
+# import pandas
+#
+# df = pandas.read_csv('data.csv')
+# print(df)
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
