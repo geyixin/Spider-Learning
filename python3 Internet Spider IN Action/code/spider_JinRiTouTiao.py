bfa8afc9199cb903180dc963920f44324fdc74c3
Geyixin <geyixin777@163.com>
2018-10-26 09:11:31 +0800

    2018-10-26 09:11:17

---
+++ python3 Internet Spider IN Action/code/spider_JinRiTouTiao.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+__author__ = 'geyixin'
+
+import requests
+from urllib.parse import urlencode
+import os
+from hashlib import md5
+from multiprocessing.pool import Pool
+
+'''
+带有ajax的 今日头条-街拍 图片的抓取
+--为此 我的IP感觉被头条封了，，，
+--Note：我这个抓取的是大图哦，，，
+'''
+
+HEADERS = {
+    "X-Requested-With": "XMLHttpRequest",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36"
+    "(KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
+}
+
+
+def get_page(offset):
+    url = 'https://www.toutiao.com/search_content/?'
+    params = {
+        'offset': offset,
+        'format': 'json',
+        'keyword': '街拍',
+        'autoload': 'true',
+        'count': '20',
+        'cur_tab': '1',
+        'from': 'search_tab',
+    }
+
+    url = url + urlencode(params)
+
+    try:
+        response = requests.get(url, headers=HEADERS)
+        if response.status_code == 200:
+            return response.json()
+    except requests.ConnectionError:
+        return None
+
+
+def get_images(json):
+    if json.get('data'):
+        for item in json.get('data'):
+            if item.get('cell_type') is not None:
+                continue
+            title = item.get('title')
+            images = item.get('image_list')
+            for image in images:
+                # print('IMAGe:',type(image.get('url')))
+                image_url = image.get('url')
+                image_url = image_url.replace("list", "large")
+                yield {
+                    'image': 'https:' + image_url,
+                    'title': title,
+                }
+
+
+def save_image(item):
+    if not os.path.exists(item.get('title')):
+        os.mkdir(item.get('title'))
+    try:
+        # print('item_get_image:', item.get('image'))
+        response = requests.get(item.get('image'))
+        if response.status_code == 200:
+            file_path = '{0}/{1}.{2}'.format(item.get('title'),
+                                           md5(response.content).hexdigest(),
+                                           'jpg')
+            print('file_path:',file_path)
+            if not os.path.exists(file_path):
+                with open(file_path, 'wb') as f:
+                    f.write(response.content)
+            else:
+                print('Already Downloaded.')
+    except requests.ConnectionError:
+        print('Failed to save image')
+
+
+def main(offset):
+    json = get_page(offset)
+    for item in get_images(json):
+        print(item)
+        save_image(item)
+
+
+GROUP_START = 1
+GROUP_END = 5
+
+
+if __name__ == '__main__':
+    pool = Pool()
+    groups = ([x*20 for x in range(GROUP_START, GROUP_END+1)])
+    print(groups)
+    pool.map(main, groups)
+    pool.close()
+    pool.join()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
